d8efe8ba701ced70a3590524fc4d7af775296305
irubido <ivan.rubido@gmail.com>
2022-11-22 14:43:34 +0100

    error integration test

---
+++ packages/error/test/integration/index.spec.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import {
+  Dappeteer,
+  initSnapEnv,
+  DappeteerPage,
+  DappeteerBrowser,
+} from '@chainsafe/dappeteer';
+
+describe('error snap', function () {
+  let dappeteer: Dappeteer;
+  let browser: DappeteerBrowser;
+  let connectedPage: DappeteerPage;
+  let snapId: string;
+
+  beforeAll(async function () {
+    ({ dappeteer, snapId, browser } = await initSnapEnv({
+      automation: 'playwright',
+      browser: 'chrome',
+      snapIdOrLocation: path.resolve(__dirname, '../..'),
+      hasPermissions: true,
+      hasKeyPermissions: false,
+    }));
+    connectedPage = await dappeteer.page.browser().newPage();
+    await connectedPage.goto('https://google.com');
+  });
+
+  afterAll(async function () {
+    browser.close();
+  });
+
+  test('snap error on invoke snap', async function () {
+    const resultPromise = dappeteer.snaps.invokeSnap(
+      connectedPage,
+      snapId,
+      'rpc',
+    );
+    const result = await resultPromise;
+    expect(result).toStrictEqual('foo');
+  });
+});
